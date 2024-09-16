410d8a1688f561e2b010e6d4da80586869160086
Abhinav <abhi06991@gmail.com>
2024-09-16 15:02:02 +0530

    Added manifest files and other code changes

--- AppTwo.java
+++ AppTwo.java
@@ -1,5 +1,27 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class AppTwo {
-    public static void main(String[] args) {
-        System.out.println("“When in doubt, fish!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("Server started at http://localhost:8080");
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "When in doubt, fish!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM eclipse-temurin:22.0.2_9-jdk-alpine
+FROM eclipse-temurin:22.0.2_9-jdk-ubi9-minimal
 
 COPY AppTwo.java /usr/src/triv-app2/
 
 WORKDIR /usr/src/triv-app2
 
 RUN javac AppTwo.java
+
+EXPOSE 8080
 
 CMD ["sh", "-c", "java AppTwo && tail -f /dev/null"]
